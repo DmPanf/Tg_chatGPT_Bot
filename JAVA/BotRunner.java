c02c4381b1e62b3428e8195e8316679deaa74c0a
Dmitrii Panfilov <99917230+DmPanf@users.noreply.github.com>
2023-08-13 22:01:15 +0300

    Create BotRunner.java

---
+++ JAVA/BotRunner.java
@@ -0,0 +1,16 @@
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class BotRunner {
+    public static void main(String[] args) {
+        ApiContextInitializer.init();
+        TelegramBotsApi botsApi = new TelegramBotsApi();
+
+        try {
+            botsApi.registerBot(new ChatGPTBot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
